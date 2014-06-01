fbae3dff17711c65d0a832494f988f61623d7e13
James Switzer <jswitzer@shastaqa.com>
2014-05-31 17:27:28 -0700

    Added more of the schema
    Added more, possibly the rest of the schema to schema.sql. Also added a handy diagram using the highest fidelity ascii art

--- schema.sql
+++ schema.sql
@@ -1,31 +1,114 @@
 CREATE TABLE Character (
-	ID INT PRIMARY KEY, Character TEXT, Player TEXT, Notes TEXT, Alias TEXT,
+	ID INT PRIMARY KEY AUTOINCREMENT, Character TEXT, Player TEXT, Notes TEXT, Alias TEXT,
 	Metatype TEXT, Ethnicity TEXT, Age INT, Sex TEXT, Height TEXT, Weight TEXT, 
 	StreetCred TEXT, Notoriety TEXT, PublicAwareness TEXT, Karma INT, TotalKarma INT, 
-	Misc TEXT, Body INT, Agility INT, Reaction INT, Strength INT, Willpower INT, 
-	Logic INT, Intuition INT, Charisma INT, Edge INT, EdgePoints INT, Essence REAL,
-	MagicResonance INT, Initiative TEXT, MatrixInitiative TEXT, AstralInitiative TEXT,
-	LifeStyle TEXT, Nuyen TEXT
+	Misc TEXT, EdgePoints INT, 	Initiative TEXT, MatrixInitiative TEXT, 
+	AstralInitiative TEXT, LifeStyle TEXT, Nuyen TEXT
+	-- Body INT, Agility INT, Reaction INT, Strength INT, Willpower INT, 
+	-- Logic INT, Intuition INT, Charisma INT, Edge INT, Essence REAL,
+	-- MagicResonance INT, 
 );
 
 CREATE TABLE Character_Quality (
-	ID INT PRIMARY KEY, CID INT, QID INT, Details TEXT, Karma INT
+	ID INT PRIMARY KEY AUTOINCREMENT, CID INT, QID INT, Details TEXT, Karma INT
 );
 
 CREATE TABLE Character_Skill (
-	ID INT PRIMARY KEY, CID INT, SID INT, Rating INT, Specialization TEXT, Notes Text
+	ID INT PRIMARY KEY AUTOINCREMENT, CID INT, SID INT, Rating INT, Specialization TEXT, 
+	Notes Text
 );
 
 CREATE TABLE Contact (
-	ID INT PRIMARY KEY, CID INT, Name TEXT, Loyalty INT, Connection INT, Favor TEXT,
-	Misc TEXT
+	ID INT PRIMARY KEY AUTOINCREMENT, CID INT, Name TEXT, Loyalty INT, Connection INT, 
+	Favor TEXT, Misc TEXT
 );
 
 CREATE TABLE Quality (
-	ID INT PRIMARY KEY, Name TEXT, Type TEXT, Cost INT, Description TEXT
+	ID INT PRIMARY KEY AUTOINCREMENT, Name TEXT, Type TEXT, Cost INT, Description TEXT
 );
 
 CREATE TABLE Skill (
-	ID INT PRIMARY KEY, Name TEXT, Type TEXT, `Default` BOOLEAN, SkillGroup INT,
-	Specializations TEXT, Description TEXT, Stat TEXT
+	ID INT PRIMARY KEY AUTOINCREMENT, Name TEXT, Type TEXT, `Default` BOOLEAN, 
+	SkillGroup INT, Specializations TEXT, Description TEXT, Stat TEXT
 );
+
+CREATE TABLE Deck (
+	ID INT PRIMARY KEY AUTOINCREMENT, CID INT, Model TEXT, Attack INT, Sleaze INT, 
+	DeviceRating INT, DataProcessing INT, Firewall INT, MatrixDmg INT, MaxMatrixDmg INT
+);
+
+CREATE TABLE Deck_Program (
+	ID INT PRIMARY KEY AUTOINCREMENT, DID INT, PID INT, Rating INT
+);
+
+CREATE TABLE Program (
+	ID INT PRIMARY KEY AUTOINCREMENT, Name TEXT, `Desc` TEXT, Cost INT
+);
+
+CREATE TABLE Vehicle (
+	ID INT PRIMARY KEY AUTOINCREMENT, CID INT, Name TEXT, Acceleration INT, Pilot INT, 
+	Armor INT, Hadling TEXT, Speed TEXT, Body INT, Sensor INT, Notes TEXT
+);
+
+CREATE TABLE Gear (
+	ID INT PRIMARY KEY AUTOINCREMENT, CID INT, Name TEXT, `Desc` TEXT, Rating INT, 
+	Count INT, Active BOOLEAN
+);
+
+CREATE TABLE RangedWeapon (
+	ID INT PRIMARY KEY AUTOINCREMENT, GID INT, Mode INT, Dmg TEXT, Acc TEXT, AP INT, RC INT,
+	Ammo INT, ClipSize TEXT
+);
+
+CREATE TABLE Mode (
+	ID INT PRIMARY KEY AUTOINCREMENT, Name TEXT, `Desc` TEXT, Ammo INT
+);
+
+CREATE TABLE Weapon_Mode (
+	ID INT PRIMARY KEY AUTOINCREMENT, WID INT, MID INT
+);
+
+CREATE TABLE MeleeWeapon (
+	ID INT PRIMARY KEY AUTOINCREMENT, GID INT, Dmg TEXT, Acc TEXT, Reach INT, AP INT
+);
+
+CREATE TABLE Armor (
+	ID INT PRIMARY KEY AUTOINCREMENT, GID INT, `Set` TEXT, Notes TEXT
+);
+
+CREATE TABLE Spells (
+	ID INT PRIMARY KEY AUTOINCREMENT, Name TEXT, Type TEXT, Target TEXT, Range TEXT, 
+	Duration TEXT, Drain TEXT
+);
+
+CREATE TABLE Character_Spell (
+	ID INT PRIMARY KEY AUTOINCREMENT, CID INT, SID INT
+);
+
+CREATE TABLE Powers (
+	ID INT PRIMARY KEY AUTOINCREMENT, Name TEXT, Rating INT, Notes TEXT
+);
+
+CREATE TABLE Character_Augmentation (
+	ID INT PRIMARY KEY AUTOINCREMENT, GID INT, AID INT, Ess REAL
+);
+
+CREATE TABLE Augmentation (
+	ID INT PRIMARY KEY AUTOINCREMENT, Name TEXT, `Desc` TEXT, MaxRating INT, Capacity INT, 
+	Avalibility TEXT, Cost INT, Type TEXT
+);
+
+CREATE TABLE Augmentation_Stat (
+	ID INT PRIMARY KEY AUTOINCREMENT, AID INT, SID INT, Rating INT
+);
+
+CREATE TABLE Stat (
+	ID INT PRIMARY KEY AUTOINCREMENT, Body INT, Agility INT, Reaction INT, Strength INT, 
+	Willpower INT, Logic INT, Intuition INT, Charisma INT, Edge INT, Essence REAL, 
+	MagicResonance INT
+);
+
+CREATE TABLE Character_Stat (
+	ID INT PRIMARY KEY AUTOINCREMENT, CID INT, SID INT, Active BOOLEAN
+);
+
